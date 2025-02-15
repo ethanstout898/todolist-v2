be7063fd4f87c8ab20d3532e38d8d9482d1d191a
Ethan Stout <ethanstout898@gmail.com>
2025-02-14 23:15:33 -0600

    Update Dependencies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const punycode = require("punycode/");
 const _ = require("lodash");
 require("dotenv").config();
 
